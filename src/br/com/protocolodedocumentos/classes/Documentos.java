ef6f494f98fd2479207fcc6b54fb023a1cce2758
sandrotimmy <smachado.ti@gmail.com>
2016-03-30 15:25:45 -0300

    subindo primeiros Arquivos

---
+++ src/br/com/protocolodedocumentos/classes/Documentos.java
@@ -0,0 +1,103 @@
+package br.com.protocolodedocumentos.classes;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Documentos implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_Documentos")
+	@SequenceGenerator(name = "seq_Documentos", sequenceName = "s_Documentos")
+    private Long id;
+    @Column(length = 50, nullable = false)
+    private String nome;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "protocolo", foreignKey = @ForeignKey(name = "fk_documentos_protocolo"))
+    private Protocolo protocolo;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "classificacao", foreignKey = @ForeignKey(name = "fk_documentos_classificacao"))
+    private Classificacao classificacao;
+
+    public Documentos() {
+    }
+
+    public Documentos(String nome, Protocolo protocolo, Classificacao classificacao) {
+        this.nome = nome;
+        this.protocolo = protocolo;
+        this.classificacao = classificacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Protocolo getProtocolo() {
+        return protocolo;
+    }
+
+    public void setProtocolo(Protocolo protocolo) {
+        this.protocolo = protocolo;
+    }
+
+    public Classificacao getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(Classificacao classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Documentos other = (Documentos) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/br/com/protocolodedocumentos/classes/Main.java
@@ -0,0 +1,24 @@
+
+package br.com.protocolodedocumentos.classes;
+
+import javax.swing.*;
+import java.sql.SQLException;
+import javax.persistence.EntityManager;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, SQLException {
+
+        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        EntityManager em = ConexaoEntityManager.getInstance();
+//        em.getTransaction();
+//        em.persist(new Requerente (1,"teste", "teste"));
+//        em.getTransaction().commit();
+//        HistoricoPadrao historico = new HistoricoPadrao(105,"teste","teste");
+//        em.getTransaction().begin();
+//        em.persist(historico);
+//        em.getTransaction().commit();
+//        JOptionPane.showMessageDialog(null, "Historico Cadastrado com Sucesso!");
+        System.exit(0);
+    }
+}
