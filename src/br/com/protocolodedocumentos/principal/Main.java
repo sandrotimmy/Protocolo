7afd519ef81d360cf8cbc2c7b24096fdcad55263
sandrotimmy <smachado.ti@gmail.com>
2016-04-06 20:06:00 -0300

    inserção no banco

--- src/br/com/protocolodedocumentos/principal/Main.java
+++ src/br/com/protocolodedocumentos/principal/Main.java
@@ -23,46 +23,46 @@ public class Main {
         UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         EntityManager em = ConexaoEntityManager.getInstance();
         
-        JAXBContext jc = JAXBContext.newInstance(Requerente.class);
-        
-        SchemaOutputResolver sor = new MySchemaOutputResolver ();
-        
-        jc.generateSchema(sor);
-        
-        
+//        JAXBContext jc = JAXBContext.newInstance(Requerente.class);
+//        
+//        SchemaOutputResolver sor = new MySchemaOutputResolver ();
+//        
+//        jc.generateSchema(sor);
         
         
         
-//        //Objeto que vai receber os valores
-//        Protocolo protocolo = new Protocolo(new Date(), "Sim", "Documentos originais");
-//                
-//        //Lista que vai receber os documentos
-//        ArrayList<Documentos> listaDocumentos = new ArrayList();
-//        //adicionando documentos na lista
-//        listaDocumentos.add(new Documentos(protocolo, "Empresarial", "Contrato Social", "Empresa Fulano de Tal LTDA"));
-//        listaDocumentos.add(new Documentos(protocolo, "Pessoal", "Documento Identidade", "Socio fulano de Tal"));
-//        listaDocumentos.add(new Documentos(protocolo, "Imposto", "SIMPLES NACIONAL", "Competencia 04/2016"));
-//        //seta os documentos no protocolo
-//        protocolo.setDocumentos(listaDocumentos);
-//        //Instancia o objeto requerente
-//        Requerente requerente = new Requerente(4568, "Sandro Machado", "Tec.Contabil", protocolo);
-//        //seta o requerente no protocolo
-//        protocolo.setRequerente(requerente);
-//        //instancia a empresa
-//        Empresa empresa = new Empresa(12548, "EMPRESA DE FAIXADA LTDA", "Industria", protocolo);
-//        //seta a empresa no protocolo
-//        protocolo.setEmpresa(empresa);
-//
-//        //persistindo os dados
-//        em.getTransaction().begin();
-//
-//        em.persist(empresa);
-//        em.persist(requerente);
-//        em.persist(protocolo);
-//        for (Documentos listaDocumento : listaDocumentos) {
-//            em.persist(listaDocumento);
-//        }
-//        em.getTransaction().commit();
+        
+        
+        //Objeto que vai receber os valores
+        Protocolo protocolo = new Protocolo(new Date(), "Sim", "Documentos originais");
+                
+        //Lista que vai receber os documentos
+        ArrayList<Documentos> listaDocumentos = new ArrayList();
+        //adicionando documentos na lista
+        listaDocumentos.add(new Documentos(protocolo, "Empresarial", "Contrato Social", "Empresa Fulano de Tal LTDA"));
+        listaDocumentos.add(new Documentos(protocolo, "Pessoal", "Documento Identidade", "Socio fulano de Tal"));
+        listaDocumentos.add(new Documentos(protocolo, "Imposto", "SIMPLES NACIONAL", "Competencia 04/2016"));
+        //seta os documentos no protocolo
+        protocolo.setDocumentos(listaDocumentos);
+        //Instancia o objeto requerente
+        Requerente requerente = new Requerente(4568, "Sandro Machado", "Tec.Contabil", protocolo);
+        //seta o requerente no protocolo
+        protocolo.setRequerente(requerente);
+        //instancia a empresa
+        Empresa empresa = new Empresa(12548, "EMPRESA DE FAIXADA LTDA", "Industria", protocolo);
+        //seta a empresa no protocolo
+        protocolo.setEmpresa(empresa);
+
+        //persistindo os dados
+        em.getTransaction().begin();
+
+        em.persist(empresa);
+        em.persist(requerente);
+        em.persist(protocolo);
+        for (Documentos listaDocumento : listaDocumentos) {
+            em.persist(listaDocumento);
+        }
+        em.getTransaction().commit();
 
         System.exit(0);
     }
